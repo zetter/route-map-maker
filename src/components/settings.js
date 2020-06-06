471bdd06cd4fa69dac7d53aba7e8309e105afc78
Chris Zetter <zetter@users.noreply.github.com>
2020-06-06 11:53:28 +0100

    Improve settings panel

--- src/components/settings.js
+++ src/components/settings.js
@@ -28,15 +28,15 @@ const Settings = ({initialX, initialY, onSubmit, show}) => {
   return <div>
     <h3>Settings</h3>
 
-    <p>The size of the repeated pattern:</p>
+    <p>The size of the repeated pattern.</p>
     <form onSubmit={submit}>
       <div>
         <label htmlFor="x">Tiles wide:</label>
-        <input name="x" value={x} onChange={(e) => setX(e.target.value)}/>
+        <input type="text" id="x" value={x} onChange={(e) => setX(e.target.value)}/>
       </div>
       <div>
         <label htmlFor="y">Tiles high:</label>
-        <input name="y" value={y} onChange={(e) => setY(e.target.value)}/>
+        <input type="text" id="y" value={y} onChange={(e) => setY(e.target.value)}/>
       </div>
 
       <input type="submit" value="Regenerate"/>
